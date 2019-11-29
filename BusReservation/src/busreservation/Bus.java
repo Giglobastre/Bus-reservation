54c2d274bc0ed591c2e7e3d34ce99cbb5182018c
Wubenji <47519360+Wubenji@users.noreply.github.com>
2019-11-29 16:44:50 +0100

    prj

---
+++ BusReservation/src/busreservation/Bus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busreservation;
+
+/**
+ *
+ * @author Benjamin WU
+ */
+public class Bus {
+    
+    //declaration des attribues
+    private int busNumber;
+    private String driverName;
+    private int hourDep,minuteDep;
+    private int hourArrival,minuteArrival;
+    private String destination;
+    private String departure;
+    
+    
+}
---
+++ BusReservation/src/busreservation/company.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busreservation;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Benjamin WU
+ */
+public class company {
+    
+    private ArrayList<customer> customerList = new ArrayList<customer>();
+    private ArrayList<Bus> BusList = new ArrayList<Bus>();
+    private ArrayList<reservation> reservationList = new ArrayList<reservation>();
+    
+}
